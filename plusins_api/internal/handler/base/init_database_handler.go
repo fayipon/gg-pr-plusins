93b7bcbb47189498fc5d0632f1349c544bdfe068
Fayi <fayi.pon@gmail.com>
2025-10-08 15:37:53 +0800

    fix i18n build error

--- plusins_api/internal/handler/base/init_database_handler.go
+++ plusins_api/internal/handler/base/init_database_handler.go
@@ -4,7 +4,6 @@ import (
 	"github.com/casbin/casbin/v2"
 	"github.com/zeromicro/go-zero/rest"
 
-	"github.com/suyuan32/simple-admin-common/i18n"
 	"github.com/fayipon/gg-pr-plusins/plusins_api/internal/config"
 	"github.com/fayipon/gg-pr-plusins/plusins_api/internal/middleware"
 )
@@ -13,7 +12,6 @@ type ServiceContext struct {
 	Config    config.Config
 	Casbin    *casbin.Enforcer
 	Authority rest.Middleware
-	Trans     *i18n.Translator
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
@@ -24,16 +22,9 @@ func NewServiceContext(c config.Config) *ServiceContext {
 		c.RedisConf,
 	)
 
-	// ✅ 新增這行：初始化 Translator，避免 nil pointer
-	var trans *i18n.Translator
-	if c.I18nConf.Dir != "" {
-		trans = i18n.NewTranslator(c.I18nConf)
-	}
-
 	return &ServiceContext{
 		Config:    c,
 		Casbin:    cbn,
 		Authority: middleware.NewAuthorityMiddleware(cbn, rds).Handle,
-		Trans:     trans,
 	}
 }
