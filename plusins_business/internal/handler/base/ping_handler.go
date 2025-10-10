5c2846f0b557864ee568522eaa7c09d359ec063a
Fayi <fayi.pon@gmail.com>
2025-10-10 17:20:31 +0800

    ent 生成CURD

--- plusins_business/internal/handler/base/ping_handler.go
+++ plusins_business/internal/handler/base/ping_handler.go
@@ -4,14 +4,14 @@ import (
 	"net/http"
 
 	"github.com/zeromicro/go-zero/rest/httpx"
+	"github.com/fayipon/gg-pr-plusins/plusins_business/internal/logic"
 	"github.com/fayipon/gg-pr-plusins/plusins_business/internal/svc"
 )
 
-// PingHandler 测试接口
 func PingHandler(ctx *svc.ServiceContext) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		httpx.OkJson(w, map[string]string{
-			"message": "pong",
-		})
+		l := logic.NewPingLogic(r.Context(), ctx)
+		result := l.Ping()
+		httpx.OkJson(w, result)
 	}
 }
