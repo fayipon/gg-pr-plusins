70cef65edb2530808d796f09ab8728c31c555b12
Fayi <fayi.pon@gmail.com>
2025-10-10 12:10:57 +0800

    Update service_context.go

--- plusins_business/internal/svc/service_context.go
+++ plusins_business/internal/svc/service_context.go
@@ -3,24 +3,19 @@ package svc
 import (
 	"github.com/fayipon/gg-pr-plusins/plusins_business/internal/config"
 	"github.com/fayipon/gg-pr-plusins/plusins_business/internal/middleware"
-
 	"github.com/casbin/casbin/v2"
+	"github.com/zeromicro/go-zero/core/stores/redis"
 	"github.com/zeromicro/go-zero/rest"
 )
 
-// 全局上下文
 type ServiceContext struct {
-	Config config.Config
-	Casbin *casbin.Enforcer
-
-	BusinessAuth rest.Middleware
-	ModuleMeter  rest.Middleware
+	Config    config.Config
+	Casbin    *casbin.Enforcer
+	Authority rest.Middleware
 }
 
-// 初始化依赖注入
 func NewServiceContext(c config.Config) *ServiceContext {
-	rds := c.RedisConf.MustNewUniversalRedis()
-
+	rds := redis.MustNewRedis(c.RedisConf) // ✅ 使用 go-zero Redis
 	cbn := c.CasbinConf.MustNewCasbinWithOriginalRedisWatcher(
 		c.CasbinDatabaseConf.Type,
 		c.CasbinDatabaseConf.GetDSN(),
@@ -28,9 +23,7 @@ func NewServiceContext(c config.Config) *ServiceContext {
 	)
 
 	return &ServiceContext{
-		Config:        c,
-		Casbin:        cbn,
-		BusinessAuth:  middleware.NewBusinessAuthMiddleware().Handle,
-		ModuleMeter:   middleware.NewModuleMeterMiddleware(rds).Handle,
+		Config:    c,
+		Authority: middleware.NewModuleMeterMiddleware(cbn, rds).Handle,
 	}
 }
