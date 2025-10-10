509def342f84c81f205b244b577c3dc8edb87701
Fayi <fayi.pon@gmail.com>
2025-10-10 12:06:46 +0800

    dd

--- plusins_business/go.mod
+++ plusins_business/go.mod
@@ -5,7 +5,7 @@ go 1.25.0
 require (
 	github.com/casbin/casbin/v2 v2.127.0
 	github.com/redis/go-redis/v9 v9.14.0
-	github.com/suyuan32/simple-admin-common v1.7.3
+	github.com/suyuan32/simple-admin-common v1.7.2
 	github.com/zeromicro/go-zero v1.9.1
 )
 
--- plusins_business/internal/config/config.go
+++ plusins_business/internal/config/config.go
@@ -1,31 +1,14 @@
-package config
-
 import (
-	sacfg "github.com/suyuan32/simple-admin-common/config"
-	"github.com/suyuan32/simple-admin-common/i18n"
-	"github.com/suyuan32/simple-admin-common/plugins/casbin"
-	"github.com/zeromicro/go-zero/core/logx"
-	"github.com/zeromicro/go-zero/rest"
+    "github.com/suyuan32/simple-admin-common/config"
+    "github.com/suyuan32/simple-admin-common/plugins/casbin"
+    "github.com/zeromicro/go-zero/rest"
 )
 
 type Config struct {
-	rest.RestConf
-	Auth               rest.AuthConf
-	CROSConf           sacfg.CROSConf
-	CasbinDatabaseConf sacfg.DatabaseConf
-	RedisConf          sacfg.RedisConf
-	CasbinConf         casbin.CasbinConf
-	I18nConf           i18n.Conf
-	Log                logx.LogConf
-
-	CoreAPI struct {
-		Host      string
-		JwtSecret string
-	}
-
-	Rpc struct {
-		Endpoint string
-	}
-
-	DatabaseConf sacfg.DatabaseConf
+    rest.RestConf
+    Auth               rest.AuthConf
+    CROSConf           config.CROSConf
+    CasbinDatabaseConf config.DatabaseConf
+    RedisConf          config.RedisConf
+    CasbinConf         casbin.CasbinConf
 }
