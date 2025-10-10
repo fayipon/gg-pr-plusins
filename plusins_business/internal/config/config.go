0bf49b0a44477d7c8c50660aeac9730b45f08394
Fayi <fayi.pon@gmail.com>
2025-10-10 12:08:42 +0800

    Update config.go

--- plusins_business/internal/config/config.go
+++ plusins_business/internal/config/config.go
@@ -1,16 +1,31 @@
 package config
 
 import (
-    "github.com/suyuan32/simple-admin-common/config"
-    "github.com/suyuan32/simple-admin-common/plugins/casbin"
-    "github.com/zeromicro/go-zero/rest"
+	sacfg "github.com/suyuan32/simple-admin-common/config"
+	"github.com/suyuan32/simple-admin-common/plugins/casbin"
+	"github.com/zeromicro/go-zero/core/logx"
+	"github.com/zeromicro/go-zero/rest"
 )
 
+// ✅ Plusins Business Service Config
 type Config struct {
-    rest.RestConf
-    Auth               rest.AuthConf
-    CROSConf           config.CROSConf
-    CasbinDatabaseConf config.DatabaseConf
-    RedisConf          config.RedisConf
-    CasbinConf         casbin.CasbinConf
+	rest.RestConf
+	Auth               rest.AuthConf
+	CROSConf           sacfg.CROSConf
+	CasbinDatabaseConf sacfg.DatabaseConf
+	RedisConf          sacfg.RedisConf
+	CasbinConf         casbin.CasbinConf
+	I18nConf           sacfg.I18nConf // ⚠️ 若 i18n 报错，可改为删除此行
+	Log                logx.LogConf
+
+	CoreAPI struct {
+		Host      string
+		JwtSecret string
+	}
+
+	Rpc struct {
+		Endpoint string
+	}
+
+	DatabaseConf sacfg.DatabaseConf
 }
